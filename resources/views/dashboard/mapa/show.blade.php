0ae45be1f2cfe20d33b6cac55b5d64c8369452f9
avancinirf <avancini.rf@gmail.com>
2021-10-21 15:05:42 +0100

    removi efeito popup do mapa e inseri modal para exibir os dados das geometrias

--- resources/views/dashboard/mapa/show.blade.php
+++ resources/views/dashboard/mapa/show.blade.php
@@ -25,27 +25,44 @@
     </div>
 
 
-
-
-    <!-- Modal -->
+    <!-- Modal Menu-->
     <div class="modal left fade" id="modal-menu-mapa" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
         <div class="modal-dialog modal-fullscreen">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="staticBackdropLabel">{{ $projeto->nome }}</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                        <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div id="modal-menu-mapa-body" class="modal-body">
-                    <p class=""><u><i><b>Nome:</b></i></u> {{ $projeto->nome }} </p>
-                    <p class=""><u><i><b>Descrição:</b></i></u> {{ $projeto->descricao }}</p>
+                    <label class="modal-menu-mapa-body-subtitulo"><b>DADOS DO PROJETO</b></label>
+                    <label class="modal-menu-mapa-body-label"><b>NOME:</b> {{ $projeto->nome }} </label>
+                    <label class="modal-menu-mapa-body-label"><b>DESCRIÇÃO:</b> {{ $projeto->descricao }}</label>
                     @if($projeto->iniciado_em || $projeto->finalizado_em)
-                    <p class=""><u><i><b>Período:</b></i></u> {{ $projeto->iniciado_em->format('d/m/Y') }} <b>até</b> {{ $projeto->finalizado_em->format('d/m/Y') }} </p>
+                    <label class="modal-menu-mapa-body-label"><b>INICIADO EM:</b> {{ $projeto->iniciado_em->format('d/m/Y') }} </label>
+                    <label class="modal-menu-mapa-body-label"><b>FINALIZADO EM:</b> {{ $projeto->finalizado_em->format('d/m/Y') }} </label>
                     @endif
-                    <br><br><br><br>
-                    <hr>
-                    {{$projeto}}
+                    <label class="modal-menu-mapa-body-label"><b>ÚLTIMA ATUALIZAÇÃO:</b> {{ $projeto->updated_at->format('d/m/Y') }} </label>
+
+                    <br><br>
+                    @if(count($projeto->arquivos) > 0)
+                        <label class="modal-menu-mapa-body-subtitulo"><b>ARQUIVOS RELACIONADOS</b></label>
+                        <ul class="map-menu-list">
+                            @foreach($projeto->arquivos as $arquivo)
+                                <a href="/app/arquivo/download/{{$arquivo->id}}"><li><i class="bi bi-box-arrow-down"></i> {{$arquivo->nome}} </li></a>
+                            @endforeach
+                        </ul>
+                    @endif
+                    @if(count($projeto->geometrias) > 0)
+                        <label class="modal-menu-mapa-body-subtitulo"><b>GEOMETRIAS RELACIONADOS</b></label>
+                        <ul class="map-menu-list">
+                            @foreach($projeto->geometrias as $geometria)
+                                <a href="/app/GEOMETRIA/download/{{$geometria->id}}"><li><i class="bi bi-box-arrow-down"></i> {{$geometria->nome}} </li></a>
+                            @endforeach
+                        </ul>
+                    @endif
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-sm btn-secondary btn-block" data-dismiss="modal">Fechar</button>
@@ -53,6 +70,26 @@
             </div>
         </div>
     </div>
+    <!-- FIM Modal Menu-->
+
+
+    <!-- Modal Informações do Layer -->
+    <div class="modal fade" id="modal-layer-mapa" tabindex="-1" aria-labelledby="modal-layer-mapa" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modal-layer-mapa-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="modal-layer-mapa-body">
+                ...
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- FIM Modal Informações do Layer -->
 
 
     <!--<script>
